7b642149d305027348527ef1d51ac4a97e7e9c6a
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2023-03-25 11:46:41 +0700

    update background and responsive navbar

--- assets/script.js
+++ assets/script.js
@@ -6,15 +6,3 @@ menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
   stopScroll.classList.toggle("stop-scrolling");
 });
-var flkty = new Flickity(".main-gallery", {
-  // options
-  cellAlign: "left",
-  contain: true,
-});
-
-// let mainBg = document.getElementById('parallax');W
-
-// window.addEventListener('scroll', function () {
-//     let value = window.scrollY;
-//     mainBg.style.top = value * 0.4 + 'px';
-// });
